5738afdc342d06e833c06e8ac7736a544e7a7d60
alsushkaa <~>
2025-02-20 13:30:08 +0300

    fix hand button

--- src/Components/HandButton/HandButton.tsx
+++ src/Components/HandButton/HandButton.tsx
@@ -4,26 +4,22 @@ import hand from '../../Assets/hand.png';
 
 import styles from './HandButton.module.css';
 import { ELEMENT_IDS } from '../../Shared/constants';
-import { NavLink } from 'react-router-dom';
 import { scrollIntoView } from '../../Utils/scrollIntoView';
 
 function HandButton({ path, className, style }: HandButtonProps) {
-	const formattedPath = `#${path}`;
-
 	const handleClick = () => {
 		scrollIntoView(path);
 	};
 	return (
-		<NavLink
+		<button
 			className={clsx(styles.hand, className)}
 			id={ELEMENT_IDS.IntroHand}
-			to={formattedPath}
 			key={path}
 			style={style}
 			onClick={handleClick}
 		>
 			<img src={hand} alt="hand" className={styles.hand_icon} />
-		</NavLink>
+		</button>
 	);
 }
 
